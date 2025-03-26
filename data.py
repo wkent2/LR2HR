Fix for data normalization

data.py
@@ -238,21 +238,15 @@ class Microstructures(Dataset):
     def __len__(self):
         return len(self.X)
 
-    # def norm_images(self, X,contrast):
-
-    #     if contrast == 'A':
-    #         a = 13500
-    #         b = 55500
-    #     elif contrast == 'C':
-    #         a = 13500
-    #         b = 76500
-    #     return (((X - a) / b - a), a, b)
-
     def norm_images(self, X,contrast):
 
-        a=np.amin(X)
-        b=np.amax(X)
-        return ((X - a) / b - 0.5, a, b)
+        if contrast == 'A':
+            a = 13500
+            b = 55500
+        elif contrast == 'C':
+            a = 13500
+            b = 76500
+        return ((X - a)/(b - a), a, b)
 
     def __getitem__(self, idx):
         x, y = self.X[idx], self.y[idx]