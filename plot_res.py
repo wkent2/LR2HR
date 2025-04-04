7785170924b0576d945a5d21463a1b0d1052ccaa
William Kent <williamkent@Williams-MacBook-Pro-6.local>
2025-04-04 18:24:17 -0400

    Added dataset printing in plot_res.py

--- plot_res.py
+++ plot_res.py
@@ -107,11 +107,13 @@ if __name__ == "__main__":
     if args.data:
         hparams['data_path'] = args.data
 
+    print("Constructing dataset from",hparams['data_path'], "with",hparams['contrast'],"contrast".)
+
     # Load data
     full_dataset = Microstructures(hparams['data_path'], 
                                     hparams['output_val'],
                                     transform=hparams['transform'],
-                                    augment=hparams['augment'],
+                                    augment=False,
                                     factor=hparams['aug_factor'],
                                     remove_bad=True,
                                     contrast=hparams['contrast'],
